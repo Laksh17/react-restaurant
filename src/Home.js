29f33091d953b01f4d3569ae59beb6e861fa6e55
Laksh <ssglaksh17@gmail.com>
2022-01-29 21:30:07 +0530

    Initial Commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+
+const Home = () => {
+
+    return (
+        <div className="home">
+            <div className="background-image-home"></div>
+            {/* <div id="color-overlay"></div> */}
+            <div className="home-container">
+                <h2>Reactive Restaurant</h2>
+                <p>First Dine, Then React</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
